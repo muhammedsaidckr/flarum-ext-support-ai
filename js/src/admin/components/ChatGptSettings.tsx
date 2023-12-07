ec1c8171dd03a5a1a35fc01f3e65c96f61c91ff3
muhammedsaidckr <muhammedsaidckr@gmail.com>
2023-12-07 10:34:32 +0300

    Updated settings name

--- js/src/admin/components/ChatGptSettings.tsx
+++ js/src/admin/components/ChatGptSettings.tsx
@@ -13,57 +13,57 @@ export default class ChatGptSettings extends ExtensionPage {
                 <div className="container">
                     <div className="Form">
                         {this.buildSettingComponent({
-                            setting: 'msc-chatgpt.api_key',
+                            setting: 'muhammedsaidckr/support-ai.api_key',
                             type: 'text',
-                            label: app.translator.trans('msc-chatgpt.admin.settings.api_key_label'),
-                            help: app.translator.trans('msc-chatgpt.admin.settings.api_key_help', {
+                            label: app.translator.trans('muhammedsaidckr/support-ai.admin.settings.api_key_label'),
+                            help: app.translator.trans('muhammedsaidckr/support-ai.admin.settings.api_key_help', {
                                 a: <a href="https://platform.openai.com/account/api-keys" target="_blank" rel="noopener"/>,
                             }),
                             placeholder: 'sk-...',
                         })}
                         {this.buildSettingComponent({
-                            setting: 'msc-chatgpt.model',
+                            setting: 'muhammedsaidckr/support-ai.model',
                             type: 'dropdown',
                             options: {
                                 'gpt-3.5-turbo-instruct': 'gpt-3.5-turbo-instruct'
                             },
-                            label: app.translator.trans('msc-chatgpt.admin.settings.model_label'),
-                            help: app.translator.trans('msc-chatgpt.admin.settings.model_help', {
+                            label: app.translator.trans('muhammedsaidckr/support-ai.admin.settings.model_label'),
+                            help: app.translator.trans('muhammedsaidckr/support-ai.admin.settings.model_help', {
                                 a: <a href="https://platform.openai.com/docs/models/overview" target="_blank" rel="noopener"/>,
                             }),
                         })}
                         {this.buildSettingComponent({
-                            setting: 'msc-chatgpt.max_tokens',
+                            setting: 'muhammedsaidckr/support-ai.max_tokens',
                             type: 'number',
-                            label: app.translator.trans('msc-chatgpt.admin.settings.max_tokens_label'),
-                            help: app.translator.trans('msc-chatgpt.admin.settings.max_tokens_help', {
+                            label: app.translator.trans('muhammedsaidckr/support-ai.admin.settings.max_tokens_label'),
+                            help: app.translator.trans('muhammedsaidckr/support-ai.admin.settings.max_tokens_help', {
                                 a: <a href="https://help.openai.com/en/articles/4936856" target="_blank" rel="noopener"/>,
                             }),
                             default: 100,
                         })}
                         {this.buildSettingComponent({
-                            setting: 'msc-chatgpt.user_prompt',
+                            setting: 'muhammedsaidckr/support-ai.user_prompt',
                             type: 'text',
-                            label: app.translator.trans('msc-chatgpt.admin.settings.user_prompt_label'),
-                            help: app.translator.trans('msc-chatgpt.admin.settings.user_prompt_help'),
+                            label: app.translator.trans('muhammedsaidckr/support-ai.admin.settings.user_prompt_label'),
+                            help: app.translator.trans('muhammedsaidckr/support-ai.admin.settings.user_prompt_help'),
                         })}
                         {this.buildSettingComponent({
-                            setting: 'msc-chatgpt.user_prompt_badge_text',
+                            setting: 'muhammedsaidckr/support-ai.user_prompt_badge_text',
                             type: 'text',
-                            label: app.translator.trans('msc-chatgpt.admin.settings.user_prompt_badge_label'),
-                            help: app.translator.trans('msc-chatgpt.admin.settings.user_prompt_badge_help'),
+                            label: app.translator.trans('muhammedsaidckr/support-ai.admin.settings.user_prompt_badge_label'),
+                            help: app.translator.trans('muhammedsaidckr/support-ai.admin.settings.user_prompt_badge_help'),
                         })}
                         {this.buildSettingComponent({
-                            setting: 'msc-chatgpt.enable_on_discussion_started',
+                            setting: 'muhammedsaidckr/support-ai.enable_on_discussion_started',
                             type: 'boolean',
-                            label: app.translator.trans('msc-chatgpt.admin.settings.enable_on_discussion_started_label'),
-                            help: app.translator.trans('msc-chatgpt.admin.settings.enable_on_discussion_started_help'),
+                            label: app.translator.trans('muhammedsaidckr/support-ai.admin.settings.enable_on_discussion_started_label'),
+                            help: app.translator.trans('muhammedsaidckr/support-ai.admin.settings.enable_on_discussion_started_help'),
                         })}
                         {this.buildSettingComponent({
                             type: 'flarum-tags.select-tags',
-                            setting: 'msc-chatgpt.enabled-tags',
-                            label: app.translator.trans('msc-chatgpt.admin.settings.enabled_tags_label'),
-                            help: app.translator.trans('msc-chatgpt.admin.settings.enabled_tags_help'),
+                            setting: 'muhammedsaidckr/support-ai.enabled-tags',
+                            label: app.translator.trans('muhammedsaidckr/support-ai.admin.settings.enabled_tags_label'),
+                            help: app.translator.trans('muhammedsaidckr/support-ai.admin.settings.enabled_tags_help'),
                             options: {
                                 requireParentTag: false,
                                 limits: {
